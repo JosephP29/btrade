6a288dbc6f781ed24b98e999ee8b04f1543b8b71
Joseph P <jwp229@gmail.com>
2017-09-14 12:32:20 -0500

    very basic profile page

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from accounts.forms import RegistrationForm
+from django.contrib.auth.models import User
 
 # Create your views here.
 def home(request):
@@ -15,3 +16,7 @@ def register(request):
         form = RegistrationForm()
         args = {'form': form}
         return render(request, 'accounts/reg_form.html', args)
+
+def profile(request):
+    args = {'user': request.user}
+    return render(request, 'accounts/profile.html', args)
