9cd43dd09797f1861d66b01bf8b54daba00784d4
Joseph P <jwp229@gmail.com>
2017-10-04 14:24:10 -0500

    moved template folder. added file for homepage

--- btrade/btrade/urls.py
+++ btrade/btrade/urls.py
@@ -18,7 +18,7 @@ from django.contrib import admin
 from btrade import views
 
 urlpatterns = [
-    url(r'^$', views.login_redirect, name="login_redirect"),
+    url(r'^$', views.index),
     url(r'^admin/', admin.site.urls),
     url(r'^account/', include('accounts.urls')),
     url(r'^leaderboard/', include('leaderboard.urls')),
--- btrade/btrade/views.py
+++ btrade/btrade/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import redirect
+from django.shortcuts import render, redirect
 
 def login_redirect(request):
     return redirect('/account/login')
+
+def index(request):
+    return render(request, 'main/index.html')
