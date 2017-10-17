3200dda9ba60501e50480d344490b6de777251f0
felixbena1 <feli_bena@hotmail.com>
2017-10-17 18:29:20 -0500

    changed buystock to stockdetail

--- btrade/stocks/urls.py
+++ btrade/stocks/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.stocks),
-    url(r'^buystock/(?P<pk>\w+)/$', views.buystock),
+    url(r'^stockdetail/(?P<pk>\w+)/$', views.stockdetail),
     url(r'^confirmbuy/(?P<pk>\w+)/$', views.confirmbuy)
 ]
