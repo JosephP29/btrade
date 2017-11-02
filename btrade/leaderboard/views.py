4bee46264783f481fa4ebccb59a9077bb899df9e
Joseph P <jwp229@gmail.com>
2017-11-02 15:59:08 -0500

    leaderboard

--- btrade/leaderboard/views.py
+++ btrade/leaderboard/views.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from accounts.models import UserProfile
 
 # Create your views here.
 def home(request):
-    return render(request, 'leaderboard/home.html')
+    users = User.objects.order_by('-userprofile__earned_currency')
+    return render(request, 'leaderboard/home.html', {'users': users})
